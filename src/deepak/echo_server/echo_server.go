575ce5f55c50fc02b8c978625802776e96085360
Deepak Kannan <kannan.deepak@gmail.com>
2013-09-30 10:35:25 +0530

    simple echo server

--- src/deepak/echo_server/echo_server.go
+++ src/deepak/echo_server/echo_server.go
@@ -1,23 +1,57 @@
+// needed to create a CLI ?
 package main
- 
+
 import (
 	"net"
-	"os"
+	"io"
+	"log"
+)
+
+// NOTE:
+// log.Fatal will log the message and calls `os.Exit(1)`
+
+// TODO:
+// SERVER_URL can be an ENV variable
+// optionally can echo to a file/stream
+// TDD ?
+// io.copy is not safe for concurrency ?
+
+const (
+	SERVER_URL = "0.0.0.0:7777"
 )
 
 func main() {
-	println("Starting the server on 0.0.0.0:7777")
-	listener, err := net.Listen("tcp", "0.0.0.0:7777")
-	println("listening")
+	listener, err := net.Listen("tcp", SERVER_URL)
+	log.Printf("listening on " + SERVER_URL)
+	
 	if err != nil {
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	
-	conn, err := listener.Accept()
-	println("accepting connection")
-	if err != nil {
-		os.Exit(1)
+	// http://golang.org/doc/effective_go.html#defer
+	// `listener.Close()` will be called finally
+	defer func(listener net.Listener) {
+		log.Printf("closing connection")
+		listener.Close()
+	}(listener)
+	
+	for {
+		conn, err := listener.Accept()
+		log.Printf("accepted connection")
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		go echoInput(conn)
 	}
-	println(conn)
 }
- 
+
+// created once per telnet session
+// http://golang.org/pkg/net/#Conn
+func echoInput(conn net.Conn) {
+	log.Printf("handeling connection")
+	// Echo all incoming data.
+	io.Copy(conn, conn)
+	// Shut down the connection.
+	conn.Close()
+}
